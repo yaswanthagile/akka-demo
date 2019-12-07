ee0dff07bbebb61061797d340285cfa10204eb4c
yaswanthagile <yaswanth.medisetti@gmail.com>
2019-12-07 15:12:37 +0530

    Added maven package that will combine all config properties for akka

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,9 @@
 package com.example;
 
 import akka.actor.typed.ActorSystem;
-
-import java.util.Scanner;
+import akka.cluster.typed.Cluster;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 
 /**
  * Hello world!
@@ -12,14 +13,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        ActorSystem<StartActor.Start> system = ActorSystem.apply(StartActor.create(), "multiply-system");
+        String[] ports = new String[]{"2552", "2551"};
+        for(String port : ports) {
+            Config config = ConfigFactory.parseString("akka.remote.artery.canonical.port=" + port).withFallback(ConfigFactory.load(App.class.getClassLoader()));
 
-
-        Scanner scanner = new Scanner(System.in);
-
-        while(scanner.hasNext()) {
-            int i = scanner.nextInt();
-            system.tell(new StartActor.InitCommand(i));
+            ActorSystem<StartActor.Start> system = ActorSystem.apply(StartActor.create(), "multiply-system", config);
+            Cluster cluster = Cluster.get(system);
+            for (int i = 3; i<20; i++) {
+                system.tell(new StartActor.InitCommand(10) {
+                });
+            }
         }
     }
 }
